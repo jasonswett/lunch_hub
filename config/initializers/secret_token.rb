9935932236fe33276e55b6709779cbb331663677
Jason Swett <jason@electricsasquatch.com>
2013-10-19 16:26:44 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LunchHub::Application.config.secret_key_base = '90ffbe57c88fa347e87375dce655b1e6b886f329974e5e1d2412cc8bd753720d6fee8900f10868b97eebb68fa070200bc13cfa57e570d216772d831dd768c2e0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LunchHub::Application.config.session_store :cookie_store, key: '_lunch_hub_session'
