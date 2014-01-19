dcd0331f29f8b48ccf77bc2020031a0906448b7b
Jason Swett <jason@electricsasquatch.com>
2014-01-19 13:00:13 -0500

    Update config to account for Bower.

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,26 @@ module LunchHub
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+   config.assets.paths << Rails.root.join('app', 'assets', 'components')
+
+    # via https://github.com/sstephenson/sprockets/issues/347#issuecomment-25543201
+
+    # We don't want the default of everything that isn't js or css, because it pulls too many things in
+    config.assets.precompile.shift
+
+    # Explicitly register the extensions we are interested in compiling
+    config.assets.precompile.push(Proc.new do |path|
+      File.extname(path).in? [
+        ".html", ".erb", ".haml",                 # Templates
+        ".png",  ".gif", ".jpg", ".jpeg", ".svg", # Images
+        ".eot",  ".otf", ".svc", ".woff", ".ttf", # Fonts
+      ]
+    end)
+
+    initializer :after_append_asset_paths, group: :all, after: :append_assets_path do
+       config.assets.paths.unshift Rails.root.join("app", "assets", "components", "jquery-ui", "themes", "base").to_s
+    end
+
     config.active_record.pluralize_table_names = false
   end
 end
