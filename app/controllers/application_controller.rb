fd3c6e9d5aca65f56cad52101bf87c3b252fc30a
Jason Swett <jason@electricsasquatch.com>
2014-07-18 19:11:06 -0400

    Add CORS config.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,19 @@
 class ApplicationController < ActionController::API
+  before_filter :set_cors_headers
+  before_filter :cors_preflight
+
   def lock_out
     redirect_to root_path unless user_signed_in?
+  end
+
+  def set_cors_headers
+    headers['Access-Control-Allow-Origin'] = AppConfig.client['origin']
+    headers['Access-Control-Allow-Methods'] = 'GET,POST,PUT,DELETE,OPTIONS'
+    headers['Access-Control-Allow-Headers'] = '*'
+    headers['Access-Control-Max-Age'] = "3628800"
+  end
+
+  def cors_preflight
+    head(:ok) if request.method == :options
   end
 end
