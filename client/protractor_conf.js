dfa1348b145f2e0f9c5dabc546d788318697975c
Jason Swett <jason@electricsasquatch.com>
2014-07-23 17:56:22 -0400

    Factor out baseUrl.

--- client/protractor_conf.js
+++ client/protractor_conf.js
@@ -1,6 +1,7 @@
 exports.config = {
   seleniumAddress: 'http://0.0.0.0:4444/wd/hub',
   capabilities: { 'browserName': 'chrome' },
+  baseUrl: 'http://127.0.0.1:9000/',
   specs: ['test/e2e/**/*.spec.js'],
   jasmineNodeOpts: {
     showColors: true,
