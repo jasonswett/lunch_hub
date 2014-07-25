eebb459216ff63b613e8baf7ce5fd56365b863a1
Jason Swett <jason@electricsasquatch.com>
2014-07-25 12:30:58 -0400

    Add comments to test-running script.

--- client/test.sh
+++ client/test.sh
@@ -1,2 +1,7 @@
+# Before running this, run node_modules/protractor/bin/webdriver-manager start.
+
+# Clean the test database.
 rake db:reset RAILS_ENV=test
+
+# Run Protractor.
 node_modules/protractor/bin/protractor protractor_conf.js
