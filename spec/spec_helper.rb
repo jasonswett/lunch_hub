076108e8633fd1c5da5f819c9c48aace19154b06
Jason Swett <jason@benfranklinlabs.com>
2014-08-29 15:26:15 -0400

    Run e2e test with RSpec.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,6 +12,8 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
+Capybara.javascript_driver = :selenium
+
 RSpec.configure do |config|
   # ## Mock Framework
   #
@@ -41,8 +43,13 @@ RSpec.configure do |config|
   config.order = "random"
 
   config.before(:suite) do
+    system("grunt build --gruntfile #{Rails.configuration.gruntfile_location}")
     DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.after(:suite) do
+    FileUtils.rm_rf(Rails.root.join("public"))
   end
 
   config.before(:each) do
