64931862ea1ab6cafb0aba34cc06396a9da49e2a
Jason Swett <jason@benfranklinlabs.com>
2014-09-23 17:40:28 -0400

    Remove wiredep cwd from Gruntfile.

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -227,7 +227,6 @@ module.exports = function (grunt) {
     // Automatically inject Bower components into the app
     wiredep: {
       options: {
-        cwd: '<%= yeoman.app %>'
       },
       app: {
         src: ['<%= yeoman.app %>/index.html'],
