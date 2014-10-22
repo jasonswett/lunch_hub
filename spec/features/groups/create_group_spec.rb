e804eee5db3bb9ee13895432b2d5f4b053f5a7a2
Jason Swett <jason@benfranklinlabs.com>
2014-10-22 08:36:03 -0400

    Change filenames from underscores to dashes.

--- spec/features/groups/create_group_spec.rb
+++ spec/features/groups/create_group_spec.rb
@@ -1,8 +1,14 @@
 require 'spec_helper'
 
-feature "Revoke admin privileges", js: true do
+feature "Add a group", js: true do
   scenario "with valid inputs" do
-    visit "/#/groups"
+		@user = FactoryGirl.create(:confirmed_user)
+    @login_page = LoginPage.new
+    @login_page.visit
+    @login_page.sign_in(@user.email, @user.password)
+    expect(page).to have_content("Today's Lunch")
+
+    visit "/groups"
     group_name = "Ben Franklin Labs"
     fill_in "group-name", with: group_name
     click_on "Save Group"
