ce56aeb2360fae7131e8ca08be0560597ef2590e
Jason Swett <jason@benfranklinlabs.com>
2014-10-24 20:28:41 -0400

    Get controller spec passing.

--- client/test/spec/controllers/announcements.spec.js
+++ client/test/spec/controllers/announcements.spec.js
@@ -1,12 +1,48 @@
 'use strict';
 
 describe('AnnouncementsCtrl', function() {
-  beforeEach(module('lunchHubApp', 'ng-token-auth'));
+  var $httpBackend,
+      announcement,
+      scope,
+      ctrl;
 
-  it('sets scope.announcements to an empty array', inject(function($controller, $rootScope) {
-    var scope = $rootScope.$new(),
-        ctrl = $controller('AnnouncementsCtrl', { $scope: scope });
+  beforeEach(function() {
+    module('lunchHubApp');
 
+    inject(function($injector) {
+      $httpBackend       = $injector.get('$httpBackend');
+      scope              = $injector.get('$rootScope').$new();
+      ctrl               = $injector.get('$controller')('AnnouncementsCtrl', { $scope: scope });
+      announcement       = { restaurantName: 'Bangkok Taste' };
+      scope.announcement = { restaurantName: 'Jason\'s Pizza' };
+
+      $httpBackend.expect('GET', '/api/announcements').respond([announcement]);
+    });
+  });
+
+  it('sets scope.announcements to an empty array', function() {
     expect(scope.announcements).toEqual([]);
-  }));
+  });
+
+  it('grabs a list of announcements', function() {
+    expect(scope.announcements.length).toBe(0);
+    $httpBackend.flush();
+    expect(scope.announcements.length).toBe(1);
+  });
+
+  describe('save', function() {
+    beforeEach(function() {
+      $httpBackend.expect('POST', '/api/announcements').respond(200, { restaurantName: 'Foo' });
+      scope.save();
+      $httpBackend.flush();
+    });
+
+    it('adds an announcement', function() {
+      expect(scope.announcements.length).toBe(2);
+    });
+
+    it('clears the restaurant name', function() {
+      expect(scope.announcement.restaurantName).toEqual('');
+    });
+  });
 });
