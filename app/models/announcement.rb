2889f70b3804840611ac0747d8cc4f82fa157d7d
Jason Swett <jason@benfranklinlabs.com>
2014-10-25 08:23:47 -0400

    Make new announcements displace any old announcements.

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -1,6 +1,7 @@
 class Announcement < ActiveRecord::Base
   belongs_to :user
   validates_presence_of :restaurant_name, :user
+  before_save -> { user.announcements.for_today.destroy_all }
 
   scope :for_today, -> {
     where("created_at >= ?", Time.zone.now.beginning_of_day)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   include DeviseTokenAuth::Concerns::User
+  has_many :announcements
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
