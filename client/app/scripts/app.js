4e7d2a9b36aa069a0e5f0cde0273c0a71cf22eb5
Jason Swett <jason@benfranklinlabs.com>
2014-10-30 10:59:26 -0400

    Refactor app.js not to create a global app variable.

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -8,78 +8,73 @@
  *
  * Main module of the application.
  */
-var app = angular.module('lunchHubApp', [
-  'ngAnimate',
-  'ngCookies',
-  'ngResource',
-  'ngRoute',
-  'ngSanitize',
-  'ngTouch',
-  'rails',
-  'ng-token-auth'
-]);
-
-app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-  $locationProvider.html5Mode(true);
-  $routeProvider
-    .when('/', {
-      templateUrl: 'views/main.html',
-      controller: 'MainCtrl'
-    })
-    .when('/today', {
-      templateUrl: 'views/announcements.html',
-      controller: 'AnnouncementsCtrl',
-      resolve: {
-        auth: ['$auth', function($auth) {
-          return $auth.validateUser();
-        }]
-      }
-    })
-    .when('/profile', {
-      templateUrl: 'views/users/edit.html',
-      controller: 'UsersCtrl',
-      resolve: {
-        auth: ['$auth', function($auth) {
-          return $auth.validateUser();
-        }]
-      }
-    })
-    .when('/groups', {
-      templateUrl: 'views/groups.html',
-      controller: 'GroupsCtrl'
-    })
-    .when('/sign_in', {
-      templateUrl: 'views/user-sessions/new.html',
-      controller: 'UserSessionsCtrl'
-    })
-    .when('/sign_up', {
-      templateUrl: 'views/user-registrations/new.html',
-      controller: 'UserRegistrationsCtrl'
-    })
-    .otherwise({
-      redirectTo: '/'
+angular.module('lunchHubApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'rails',
+    'ng-token-auth'
+  ]).
+  config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/today', {
+        templateUrl: 'views/announcements.html',
+        controller: 'AnnouncementsCtrl',
+        resolve: {
+          auth: ['$auth', function($auth) {
+            return $auth.validateUser();
+          }]
+        }
+      })
+      .when('/profile', {
+        templateUrl: 'views/users/edit.html',
+        controller: 'UsersCtrl',
+        resolve: {
+          auth: ['$auth', function($auth) {
+            return $auth.validateUser();
+          }]
+        }
+      })
+      .when('/groups', {
+        templateUrl: 'views/groups.html',
+        controller: 'GroupsCtrl'
+      })
+      .when('/sign_in', {
+        templateUrl: 'views/user-sessions/new.html',
+        controller: 'UserSessionsCtrl'
+      })
+      .when('/sign_up', {
+        templateUrl: 'views/user-registrations/new.html',
+        controller: 'UserRegistrationsCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]).
+  config(['$authProvider', function($authProvider) {
+    $authProvider.configure({
+      apiUrl: '/api'
     });
-}]);
-
-app.config(['$authProvider', function($authProvider) {
-  $authProvider.configure({
-    apiUrl: '/api'
-  });
-}]);
-
-app.factory('Announcement', ['railsResourceFactory', function (railsResourceFactory) {
-  return railsResourceFactory({ url: '/api/announcements', name: 'announcement' });
-}]);
-
-app.factory('Group', ['railsResourceFactory', function (railsResourceFactory) {
-  return railsResourceFactory({ url: '/api/groups', name: 'group' });
-}]);
-
-app.run(['$rootScope', '$location', function($rootScope, $location) {
-  $rootScope.$on('auth:login-success', function() {
-    $location.path('/today');
-  });
-  $rootScope.$on('auth:logout-success', function() {
-    $location.path('/sign_in');
-  });
-}]);
+  }]).
+  factory('Announcement', ['railsResourceFactory', function (railsResourceFactory) {
+    return railsResourceFactory({ url: '/api/announcements', name: 'announcement' });
+  }]).
+  factory('Group', ['railsResourceFactory', function (railsResourceFactory) {
+    return railsResourceFactory({ url: '/api/groups', name: 'group' });
+  }]).
+  run(['$rootScope', '$location', function($rootScope, $location) {
+    $rootScope.$on('auth:login-success', function() {
+      $location.path('/today');
+    });
+    $rootScope.$on('auth:logout-success', function() {
+      $location.path('/sign_in');
+    });
+  }]);
