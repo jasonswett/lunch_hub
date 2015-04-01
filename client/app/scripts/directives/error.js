ac169a8592c7768acafb55969fbdd2a0ad35ab83
Jason Swett <jason@benfranklinlabs.com>
2015-04-01 15:03:31 -0400

    Switch to Gulp.

--- client/app/scripts/directives/error.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('lunchHubApp')
-  .directive('error', function() {
-    return {
-      restrict: 'E',
-      templateUrl: 'views/directives/error.html'
-    };
-  });
---
+++ client/gulp/unit-tests.js
@@ -0,0 +1,63 @@
+'use strict';
+
+var gulp = require('gulp');
+
+var $ = require('gulp-load-plugins')();
+
+var wiredep = require('wiredep');
+var karma = require('karma');
+var concat = require('concat-stream');
+var _ = require('lodash');
+
+module.exports = function(options) {
+  function listFiles(callback) {
+    var bowerDeps = wiredep({
+      directory: 'bower_components',
+      exclude: [/bootstrap-sass-official/, /bootstrap\.css/],
+      dependencies: true,
+      devDependencies: true
+    });
+
+    var specFiles = [
+      options.src + '/**/*.spec.js',
+      options.src + '/**/*.mock.js'
+    ];
+
+    var htmlFiles = [
+      options.src + '/**/*.html'
+    ];
+
+    var srcFiles = [
+      options.tmp + '/serve/{app,components}/**/*.js'
+    ].concat(specFiles.map(function(file) {
+      return '!' + file;
+    }));
+
+
+    gulp.src(srcFiles)
+      .pipe($.angularFilesort()).on('error', options.errorHandler('AngularFilesort'))
+      .pipe(concat(function(files) {
+        callback(bowerDeps.js
+          .concat(_.pluck(files, 'path'))
+          .concat(htmlFiles)
+          .concat(specFiles));
+      }));
+  }
+
+  function runTests (singleRun, done) {
+    listFiles(function(files) {
+      karma.server.start({
+        configFile: __dirname + '/../karma.conf.js',
+        files: files,
+        singleRun: singleRun
+      }, done);
+    });
+  }
+
+  gulp.task('test', ['scripts'], function(done) {
+    runTests(true, done);
+  });
+  gulp.task('test:auto', ['watch'], function(done) {
+    runTests(false, done);
+  });
+};
