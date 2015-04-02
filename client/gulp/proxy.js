87b95ae3ba75cc0f3b420b7b85d711c34707507a
Jason Swett <jason@benfranklinlabs.com>
2015-04-02 07:59:53 -0400

    Fix proxy.

--- client/gulp/proxy.js
+++ client/gulp/proxy.js
@@ -21,7 +21,7 @@ var chalk = require('chalk');
 /*
  * Location of your backend server
  */
-var proxyTarget = 'http://server/context/';
+var proxyTarget = 'http://localhost:4000/';
 
 var proxy = httpProxy.createProxyServer({
   target: proxyTarget
@@ -61,7 +61,6 @@ function proxyMiddleware(req, res, next) {
  * The first line activate if and the second one ignored it
  */
 
-//module.exports = [proxyMiddleware];
 module.exports = function() {
-  return [];
+  return [proxyMiddleware];
 };
